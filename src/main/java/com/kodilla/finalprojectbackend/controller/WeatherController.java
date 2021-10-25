d48e16987e50d686233dbb6438cc2573fe3d14aa
Kamil <kamil.krol@gmail.com>
2021-10-25 19:23:34 +0200

    Adding FlightsController and WeatherController

---
+++ src/main/java/com/kodilla/finalprojectbackend/controller/WeatherController.java
@@ -0,0 +1,38 @@
+package com.kodilla.finalprojectbackend.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("v1/weather")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @GetMapping
+    public List<WeatherDto> get() {
+        return weatherService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public WeatherDto get(@PathVariable long id) {
+        return weatherService.getById(id);
+    }
+
+    @PostMapping
+    public WeatherDto create(@RequestBody WeatherDto weatherDto) {
+        return weatherService.create(weatherDto);
+    }t
+
+    @PutMapping
+    public WeatherDto update(@RequestBody WeatherDto weatherDto) {
+        return weatherService.update(weatherDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id)  {
+        weatherService.delete(id);
+    }
+}
